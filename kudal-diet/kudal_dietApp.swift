53602e9211f177bd278202b7f2f4ab395852ad7e
dlarldks <dlarldks@naver.com>
2026-06-28 23:10:53 +0900

    Initial Commit

---
+++ kudal-diet/kudal_dietApp.swift
@@ -0,0 +1,17 @@
+//
+//  kudal_dietApp.swift
+//  kudal-diet
+//
+//  Created by 미미 on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct kudal_dietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
